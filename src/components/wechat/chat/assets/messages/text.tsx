fd4c21c56ec26230a920123c93e5773455771234
liushi <liu.shi@navercorp.com>
2019-08-23 16:46:05 +0800

    add avatars

---
+++ src/components/wechat/chat/assets/messages/text.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+
+interface TextProps {
+
+}
+
+export class Text extends React.Component<{}, {}> {
+
+}
--- src/components/wechat/chat/chat.tsx
+++ src/components/wechat/chat/chat.tsx
@@ -1,19 +1,29 @@
 import * as React from "react";
 import {Phone} from "../../phone/phone";
+import {Avatar} from "../avatars/ avatars";
 
 import styles from "./assets/css/chat.module.css"
 
-export class Chat extends React.Component<{}, {}> {
+interface ChatProps {
+    user?: string;
+}
+
+export class Chat extends React.Component<ChatProps, {}> {
     public render(): React.ReactElement {
+        let chatName = this.props.user;
+        if (!chatName) {
+            chatName = "时光"
+        }
+
         return (
             <Phone>
                 <div className={styles.header}>
                     <span className={styles["back-icon"]}/>
-                    <span className={styles["chat-name"]}>头部</span>
+                    <span className={styles["chat-name"]}>{chatName}</span>
                     <span className={styles.profile}/>
                 </div>
                 <div className={styles.body}>
-                    聊天内容
+                    <Avatar size={"small"}/> 聊天内容
                 </div>
             </Phone>
         );
