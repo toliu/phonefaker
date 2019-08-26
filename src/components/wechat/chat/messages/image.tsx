f45b3f56a58b0487c8ee4d85525333f0d10a6b2b
liushi <liu.shi@navercorp.com>
2019-08-26 11:02:00 +0800

    add image type message

---
+++ src/components/wechat/chat/messages/image.tsx
@@ -0,0 +1,28 @@
+import * as React from "react";
+
+import {Mine, Other} from "./message";
+
+import styles from "../assets/css/message.module.css";
+
+interface ImageProps {
+    mine: boolean;
+    avatarURL?: string;
+    imagePath: string;
+}
+
+export class Image extends React.Component<ImageProps, {}> {
+    public render(): React.ReactElement {
+        if (this.props.mine) {
+            return (
+                <Mine avatarURL={this.props.avatarURL}>
+                    <img src={this.props.imagePath} alt={"image path"} className={styles.image}/>
+                </Mine>
+            );
+        }
+        return (
+            <Other avatarURL={this.props.avatarURL}>
+                <img src={this.props.imagePath} alt={"image path"} className={styles.image}/>
+            </Other>
+        );
+    }
+}
