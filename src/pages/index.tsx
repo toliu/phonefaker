8f0f27fbc4fd302d411ab71bc606947db182ab94
liushi <liu.shi@navercorp.com>
2019-09-02 16:52:10 +0800

    add index page

---
+++ src/pages/index.tsx
@@ -0,0 +1,57 @@
+import 'antd/dist/antd.css';
+
+import * as React from "react";
+import {Layout, Menu, Icon} from 'antd';
+
+import styles from "./assets/css/index.module.css";
+
+const {SubMenu} = Menu;
+const {Header, Content, Footer} = Layout;
+
+enum MenuKeys {
+    WechatChat = "wechat chat",
+    WechatFriends = "wechat circle of friends"
+}
+
+interface IndexStats {
+    selectedKey: MenuKeys;
+}
+
+export class Index extends React.Component<{}, IndexStats> {
+    constructor(props: any) {
+        super(props);
+        this.switchMenu = this.switchMenu.bind(this);
+
+        this.state = {
+            selectedKey: MenuKeys.WechatChat,
+        };
+    }
+
+    public render(): React.ReactElement {
+        return (
+            <Layout>
+                <Header className="header">
+                    <Menu theme="dark" mode="horizontal" defaultSelectedKeys={[this.state.selectedKey]}
+                          className={styles.menu} onSelect={this.switchMenu}>
+                        <SubMenu title={<span><Icon type="wechat"/>微信</span>}>
+                            <Menu.Item key={MenuKeys.WechatChat}>聊天</Menu.Item>
+                            <Menu.Item key={MenuKeys.WechatFriends}>朋友圈</Menu.Item>
+                        </SubMenu>
+                    </Menu>
+                </Header>
+                <Content className={styles.body}>
+                    <Layout className={styles.content}>
+                        <Content className={styles.page}>
+                            {this.state.selectedKey}
+                        </Content>
+                    </Layout>
+                </Content>
+                <Footer className={styles.footer}>Phone Faker ©2019 Created by liushi</Footer>
+            </Layout>
+        );
+    }
+
+    private switchMenu(e: any) {
+        this.setState({selectedKey: e.key})
+    }
+}
--- stories/index.stories.js
+++ stories/index.stories.js
@@ -5,6 +5,7 @@ import {action} from '@storybook/addon-actions';
 
 import {FixedPhone} from "../src/components/phone/fixedphone";
 import {FixedChat} from "../src/components/wechat/chat/fixedChat";
+import {Index} from "../src/pages";
 
 storiesOf("手机", module)
     .add("空手机", () => <FixedPhone/>)
@@ -18,4 +19,7 @@ storiesOf("微信", module)
     .add("对话", () => {
         return <FixedChat/>
     });
+
+storiesOf("首页", module)
+    .add("page", () => <Index/>);
 
