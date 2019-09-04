189bb3162ea026699a00c894c7c3f6fdc990caff
liushi <liu.shi@navercorp.com>
2019-09-04 16:23:28 +0800

    config emoji

--- src/components/wechat/chat/emoji_input.tsx
+++ src/components/wechat/chat/emoji_input.tsx
@@ -13,8 +13,27 @@ export class EmojiInput extends React.Component<EmojiInputProps, {}> {
         return (
             <Picker
                 onSelect={(ed: any) => this.props.onSelect(ed.native)}
-                native={true}
-                emojiSize={12}
+                emojiSize={16}
+                style={{
+                    maxWidth: "100%"
+                }}
+                i18n={{
+                    search: "搜索",
+                    notfound: "未找到表情",
+                    categories: {
+                        search: "搜索结果",
+                        recent: "最近使用",
+                        people: "人物",
+                        nature: "自然",
+                        foods: "食物",
+                        activity: "生活",
+                        places: "风景",
+                        objects: "物品",
+                        symbols: "符号",
+                        flags: "标志",
+                        custom: "自定义",
+                    },
+                }}
             />
         );
     }
