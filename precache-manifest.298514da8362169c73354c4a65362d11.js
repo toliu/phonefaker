d0a8489f5de8877b8d0a99b768a4e631115ff654
liushi <liu.shi@navercorp.com>
2019-09-05 13:25:33 +0800

    Updates

---
+++ precache-manifest.298514da8362169c73354c4a65362d11.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cce88638d65e09688266e198a9be1829",
+    "url": "/phone-faker/index.html"
+  },
+  {
+    "revision": "89ee26ac750ac15a91f3",
+    "url": "/phone-faker/static/css/2.3795f1f3.chunk.css"
+  },
+  {
+    "revision": "2cebbae6b443c6ace1a7",
+    "url": "/phone-faker/static/css/main.e7546b1b.chunk.css"
+  },
+  {
+    "revision": "89ee26ac750ac15a91f3",
+    "url": "/phone-faker/static/js/2.ce352162.chunk.js"
+  },
+  {
+    "revision": "2cebbae6b443c6ace1a7",
+    "url": "/phone-faker/static/js/main.995f201f.chunk.js"
+  },
+  {
+    "revision": "ac237529f9f98a7e7be7",
+    "url": "/phone-faker/static/js/runtime~main.2feec4a4.js"
+  },
+  {
+    "revision": "e616106e3a7d41191470c30a96eacb1e",
+    "url": "/phone-faker/static/media/PingFang-SC-Regular.e616106e.ttf"
+  },
+  {
+    "revision": "2d511b012c8f02fad334f301834674c1",
+    "url": "/phone-faker/static/media/default_avatar1.2d511b01.jpg"
+  },
+  {
+    "revision": "ff4149c2adb7e1eab44881f05a3ad859",
+    "url": "/phone-faker/static/media/default_avatar2.ff4149c2.jpg"
+  },
+  {
+    "revision": "8afabdb24374806baa7d9dcae65969a3",
+    "url": "/phone-faker/static/media/signal-4.8afabdb2.svg"
+  },
+  {
+    "revision": "8f524f6302eb36289407a18ec92b893f",
+    "url": "/phone-faker/static/media/wifi.8f524f63.svg"
+  }
+]);
