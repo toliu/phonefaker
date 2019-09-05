7a27bed957a654cfe5b29fc6568d28bd54c67961
liushi <liu.shi@navercorp.com>
2019-09-05 13:36:43 +0800

    Updates

---
+++ precache-manifest.a309800207cfb120f14e866814f79bd2.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64c02cf6a3f33dfa417ad01684a87f14",
+    "url": "/phonefaker/index.html"
+  },
+  {
+    "revision": "e75dd4b9b4b4b82ab698",
+    "url": "/phonefaker/static/css/2.3795f1f3.chunk.css"
+  },
+  {
+    "revision": "882a63130e82f7eeb0b9",
+    "url": "/phonefaker/static/css/main.832818c8.chunk.css"
+  },
+  {
+    "revision": "e75dd4b9b4b4b82ab698",
+    "url": "/phonefaker/static/js/2.701061e1.chunk.js"
+  },
+  {
+    "revision": "882a63130e82f7eeb0b9",
+    "url": "/phonefaker/static/js/main.e025a48b.chunk.js"
+  },
+  {
+    "revision": "c497125b76b3617e645f",
+    "url": "/phonefaker/static/js/runtime~main.5545ac6b.js"
+  },
+  {
+    "revision": "e616106e3a7d41191470c30a96eacb1e",
+    "url": "/phonefaker/static/media/PingFang-SC-Regular.e616106e.ttf"
+  },
+  {
+    "revision": "2d511b012c8f02fad334f301834674c1",
+    "url": "/phonefaker/static/media/default_avatar1.2d511b01.jpg"
+  },
+  {
+    "revision": "ff4149c2adb7e1eab44881f05a3ad859",
+    "url": "/phonefaker/static/media/default_avatar2.ff4149c2.jpg"
+  },
+  {
+    "revision": "8afabdb24374806baa7d9dcae65969a3",
+    "url": "/phonefaker/static/media/signal-4.8afabdb2.svg"
+  },
+  {
+    "revision": "8f524f6302eb36289407a18ec92b893f",
+    "url": "/phonefaker/static/media/wifi.8f524f63.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/phone-faker/precache-manifest.298514da8362169c73354c4a65362d11.js"
+  "/phonefaker/precache-manifest.a309800207cfb120f14e866814f79bd2.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/phone-faker/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/phonefaker/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
