ec8a0bfa7ce64daab580b20e35ba7479d57bf2b3
liushi <liu.shi@navercorp.com>
2019-09-17 15:15:22 +0800

    Add friend circle page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,6 +7,7 @@ import {Developing} from "./developing";
 import {Home} from "./home";
 import {Chat} from "./wechat/chat";
 import {Site, SiteKeys} from "../components/site";
+import {Friend} from "./wechat/friend";
 
 export class Index extends React.Component<{}, {}> {
     public render(): React.ReactElement {
@@ -16,7 +17,7 @@ export class Index extends React.Component<{}, {}> {
                     <Switch>
                         <Route exact={true} path={SiteKeys.Home} component={Home}/>
                         <Route exact={true} path={SiteKeys.WeChatChat} component={Chat}/>
-                        <Route exact={true} path={SiteKeys.WeChatFriends} component={Developing}/>
+                        <Route exact={true} path={SiteKeys.WeChatFriends} component={Friend}/>
                         <Redirect to={SiteKeys.Home}/>
                     </Switch>
                 </Site>
---
+++ src/pages/wechat/friend.tsx
@@ -0,0 +1,110 @@
+import * as React from "react";
+import {Row, Col, Upload, Divider, Input, Icon, Tooltip, Button} from "antd";
+
+import {FriendCircle} from "../../components/wechat/friend-cricle/friend-cricle";
+import {FriendCircleMessage} from "../../components/wechat/friend-cricle/messages";
+
+import defaultBackgroundPicture from "../../assets/img/wechat-friend-default-background.jpg";
+import defaultUserAvatar from "../../assets/img/default_avatar1.jpg";
+
+import styles from "../../assets/css/site-friend-circle.module.css";
+
+interface FriendStats {
+    userName: string;
+    userAvatar: string;
+    backgroundPicture: string;
+    messages: FriendCircleMessage[];
+}
+
+export class Friend extends React.Component<{}, FriendStats> {
+    constructor(props: any) {
+        super(props);
+        this.state = {
+            messages: [],
+            backgroundPicture: defaultBackgroundPicture,
+            userAvatar: defaultUserAvatar,
+            userName: "时光",
+        };
+    }
+
+    public render(): React.ReactElement {
+        return (
+            <React.Fragment>
+                <Row align={"middle"}>
+                    <Col span={8} offset={4}>
+                        <FriendCircle
+                            messages={this.state.messages}
+                            userName={this.state.userName}
+                            userAvatar={this.state.userAvatar}
+                            backgroundImage={this.state.backgroundPicture}
+                        />
+                    </Col>
+                    <Col span={8} order={4}>
+                        <Row align={"middle"}>
+                            <Col span={2}>
+                                <strong>用户名:</strong>
+                            </Col>
+                            <Col offset={1} span={5}>
+                                <Input
+                                    placeholder={this.state.userName}
+                                    prefix={<Icon type={"user"} style={{color: 'rgba(0,0,0,.25)'}}/>}
+                                    suffix={
+                                        <Tooltip title="用户名">
+                                            <Icon type="info-circle" style={{color: 'rgba(0,0,0,.45)'}}/>
+                                        </Tooltip>
+                                    }
+                                    onChange={(e: any) => {
+                                        this.setState({userName: e.target.value})
+                                    }}
+                                    size={"small"}
+                                />
+                            </Col>
+                        </Row>
+
+                        <Divider dashed={true}/>
+
+                        <Row align={"middle"}>
+                            <Col span={2}>
+                                <strong>背景:</strong>
+                            </Col>
+                            <Col span={10}>
+                                <Upload
+                                    name="avatar" listType="picture-card" className={styles["uploader-icon"]} showUploadList={false}
+                                    onChange={(info) => {
+                                        const reader = new FileReader();
+                                        reader.addEventListener("load", () => {
+                                            this.setState({backgroundPicture: reader.result as string});
+                                        });
+                                        reader.readAsDataURL(info.file.originFileObj as File);
+                                    }}
+                                >
+                                    <img src={this.state.backgroundPicture} alt={"背景"} className={styles["uploader-icon"]}/>
+                                </Upload>
+                            </Col>
+                            <Col span={2}>
+                                <strong>头像:</strong>
+                            </Col>
+                            <Col span={10}>
+                                <Upload
+                                    name="avatar" listType="picture-card" className={styles["uploader-icon"]} showUploadList={false}
+                                    onChange={(info) => {
+                                        const reader = new FileReader();
+                                        reader.addEventListener("load", () => {
+                                            this.setState({userAvatar: reader.result as string});
+                                        });
+                                        reader.readAsDataURL(info.file.originFileObj as File);
+                                    }}
+                                >
+                                    <img src={this.state.userAvatar} alt={"头像"} className={styles["uploader-icon"]}/>
+                                </Upload>
+                            </Col>
+                        </Row>
+                        <Divider dashed={true}/>
+
+                    </Col>
+                </Row>
+            </React.Fragment>
+        );
+    }
+
+}
